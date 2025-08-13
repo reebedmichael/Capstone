54f2746588dba10fa5abd96abaefd43ea5c630c5
JacquesSwanepoel <125281769+JacquesSwanepoel@users.noreply.github.com>
2025-08-13 15:21:57 +0200

    Hierdie is 'n toets om te sien of Jacques ordentelik kan commit

--- apps/mobile/lib/main.dart
+++ apps/mobile/lib/main.dart
@@ -11,7 +11,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Jacques se Flippen Cool skerm',
       theme: ThemeData(
         // This is the theme of your application.
         //
